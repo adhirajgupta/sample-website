948a27b5ee09d4bcf1dbcd9b622fdf92f6c5cebb
adhirajgupta <60607630+adhirajgupta@users.noreply.github.com>
2023-01-20 23:45:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<head>
+    <title> Form</title>
+    <body>
+      <div>
+        <label for="fname"> First name: </label>
+        <input type="text" id="fname "name="fname" placeholder="Anvi" required >
+      </div>
+      <br>
+
+      <div>
+        <label for="lname"> Last name: </label>
+        <input type="text" id="lname "name="lname" placeholder="Shah" required>
+      </div>
+      <br>
+
+      <div>
+        <label for="password"> password: </label>
+        <input type="password" id="password" name="password" maxlength="6" required>
+      </div>
+      
+      <br>
+
+      <div>
+        <label for="email"> email: </label>
+        <input type="email" id="email "name="email" placeholder="anvi@gmail.com" required>
+      </div>
+      <br>
+
+      <div>
+        <label for="telephone"> Phone Number:  </label>
+        <input type="tel" id="telephone "name="telephone" max="10" required>
+      </div>
+      <br>
+
+      <div>
+        <label for="bdate"> Birth date: </label>
+        <input type="date" id="bdate"name="bdate">
+      </div>
+      <br>
+
+      <div>
+        <label for="quantity"> quantity: </label>
+        <input type="number" id="quantity"name="quantity" min="0" max="10" value="1" required>
+      </div>
+      <br>
+
+      <div>
+        <label for="title"> title: </label>
+        
+        <label for="Mr."> Mr.</label>
+        <input type="radio" id="Mr." name="title" value="Mr.">
+
+
+        <label for="Mrs."> Mrs.</label>
+        <input type="radio" id="Mrs." name="title" value="Mrs.">
+
+        <label for="PhD"> PhD</label>
+        <input type="radio" id="PhD" name="title" value="PhD">
+      </div>
+      <br>
+
+      <div>
+        <label for="payment"> payment: </label>
+        <select name="payment" id="payment">
+         <option> visa </option>
+         <option> Mastercard </option>
+         <option> Debitcard  </option>
+        </select>
+      </div>
+      <br>
+      <div>
+        <input type="reset">
+      </div>
+      <br>
+      <div>
+        
+        <input type="submit">
+      </div>
+      <br>
+    </body>
+</head>
